257e8c6fe55ae3c3e0385531b3d4eb038e76d267
Kehan Ranasinghe <kehanran51423@gmail.com>
2026-04-23 18:09:09 +1000

    prep repo for test deployment

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,6 +1,10 @@
 import axios from "axios";
 
+const baseURL =
+  (import.meta.env.VITE_API_URL as string | undefined) ??
+  "http://localhost:3000/api";
+
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL,
   withCredentials: true,
 });
